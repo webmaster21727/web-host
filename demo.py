b55c091cb92ef7d8df9757d37e3fecc85f96f06a
webmaster21727 <webmaster21727@gmail.com>
2026-06-14 22:20:59 +0530

    Enhance Streamlit app with age slider and updates
    Updated the Streamlit app to include a slider for age and changed the title and item list.

--- demo.py
+++ demo.py
@@ -1,7 +1,10 @@
 import streamlit as st
 st.title("__WEBSITE__")
-st.write("hell world")
+st.write("WEB-HOSTING")
 name= st.text_input("enter name")
-st.write(name)
-b=st.selectbox("your fav item ",["phone","laptop","etc"])
+if st.button("CLICK"):
+    st.write(name)
+b=st.selectbox("your fav item ",["phone","laptop","tablete"])
 st.write("ITEM is := " + b)
+c= st.slider("your age =",1,100)
+st.write("your age:="c)
